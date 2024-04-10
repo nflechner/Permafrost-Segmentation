4e2b1545319e14466100081b53dd2501f878b78f
nflechner <60731786+nflechner@users.noreply.github.com>
2024-04-10 12:21:47 +0200

    start coding

--- data_prep/main.py
+++ data_prep/main.py
@@ -13,6 +13,8 @@ from rasterio.mask import mask
 import os
 import json
 
+# functions 
+from functions import filter_imgs
 
 ##################
 ## load configs ##
@@ -22,10 +24,14 @@ config_path = os.path.join(os.getcwd(), 'configs.json')
 with open(config_path, 'r') as config_file:
     configs = json.load(config_file)
 
-config_paths = configs.get('paths', {})
-original_tif_dir = config_paths.get('original_tif_dir')
-
 ##########
 ## code ##
 ##########
 
+# extract files containing palsa
+config_paths = configs.get('paths', {}) 
+original_tif_dir = config_paths.get('original_tif_dir') # load directory with all tifs
+palsa_tifs = filter_imgs(original_tif_dir)
+
+# load all 100x100m 'rutor' containing palsa
+palsa_shapefile_path = config_paths.get('palsa_shapefile_path') # load shapefile path
