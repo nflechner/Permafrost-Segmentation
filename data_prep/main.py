709ff66b0dc33f13b465d0f0599ade0875b1ef71
nflechner <60731786+nflechner@users.noreply.github.com>
2024-04-10 12:44:19 +0200

    make configs own section

--- data_prep/main.py
+++ data_prep/main.py
@@ -14,7 +14,7 @@ import os
 import json
 
 # functions 
-from functions import filter_imgs
+from functions import filter_imgs, Crop_tif
 
 ##################
 ## load configs ##
@@ -24,14 +24,21 @@ config_path = os.path.join(os.getcwd(), 'configs.json')
 with open(config_path, 'r') as config_file:
     configs = json.load(config_file)
 
+# load paths from configs 
+config_paths = configs.get('paths', {}) 
+palsa_shapefile_path = config_paths.get('palsa_shapefile_path') # load shapefile path
+save_crops_dir = config_paths.get('save_crops_dir') # load directory with all tifs
+original_tif_dir = config_paths.get('original_tif_dir') # load directory with all tifs
+
 ##########
 ## code ##
 ##########
 
-# extract files containing palsa
-config_paths = configs.get('paths', {}) 
-original_tif_dir = config_paths.get('original_tif_dir') # load directory with all tifs
-palsa_tifs = filter_imgs(original_tif_dir)
+# extract tif file names which contain palsa
+palsa_tifs = filter_imgs(original_tif_dir) # returns a list of filenames to be cropped
 
-# load all 100x100m 'rutor' containing palsa
-palsa_shapefile_path = config_paths.get('palsa_shapefile_path') # load shapefile path
+# load palsa shape path
+for img_path in palsa_tifs:
+    cropping = Crop_tif(img_path, palsa_shapefile_path, save_crops_dir)
+
+
