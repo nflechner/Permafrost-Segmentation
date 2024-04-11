790b6e986add6ee5d5894240435a868c8f4984c8
nflechner <60731786+nflechner@users.noreply.github.com>
2024-04-11 16:58:29 +0200

    added logger

--- data_prep/main.py
+++ data_prep/main.py
@@ -12,9 +12,27 @@ from rasterio.plot import show
 from rasterio.mask import mask
 import os
 import json
+import logging
 
 # functions 
 from functions import filter_imgs, Crop_tif
+
+
+##################
+## setup logger ##
+##################
+
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
+
+# Setup logger
+ch = logging.StreamHandler() # create console handler
+ch.setLevel(logging.DEBUG) # set level to debug
+formatter = logging.Formatter("%(asctime)s - %(message)s \n", "%Y-%m-%d %H:%M:%S") # create formatter
+ch.setFormatter(formatter) # add formatter to ch
+logger.addHandler(ch) # add ch to logger
+
+logger.info('Imports successful')
 
 ##################
 ## load configs ##
@@ -30,20 +48,29 @@ palsa_shapefile_path = config_paths.get('palsa_shapefile_path') # load shapefile
 save_crops_dir = config_paths.get('save_crops_dir') # load directory with all tifs
 original_tif_dir = config_paths.get('original_tif_dir') # load directory with all tifs
 
+logger.info('Configurations were loaded')
+
 ##########
 ## code ##
 ##########
 
+logger.info('Starting to sample relevant TIF paths...')
+
 # extract tif file names which contain palsa
 palsa_tifs = filter_imgs(original_tif_dir) # returns a list of filenames to be cropped
 
+logger.info(f'{len(palsa_tifs)} TIF paths have been loaded!')
+logger.info('Starting to generate training samples from TIFs..')
+
 # load palsa shape path
-for img_name in palsa_tifs:
+for idx, img_name in enumerate(palsa_tifs):
     img_name_code = img_name.split('.')[0]
     img_path = os.path.join(original_tif_dir, img_name)
-    cropping = Crop_tif(img_name_code, img_path, palsa_shapefile_path, save_crops_dir)
+    cropping = Crop_tif(img_name_code, img_path, palsa_shapefile_path, save_crops_dir, logger)
     positive_labels = cropping.crop_rutor()
     negative_labels = cropping.crop_negatives()
+    logger.info(f'Generated training samples from image {idx+1}/{len(palsa_tifs)}')
+
 
 
 
