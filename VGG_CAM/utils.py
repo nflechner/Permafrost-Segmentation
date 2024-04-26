23fa04a9290b437876248e9cfb4900ab8a21b0b3
nflechner <60731786+nflechner@users.noreply.github.com>
2024-04-26 13:06:12 +0200

    working training loop VGG CAM

--- VGG_CAM/utils.py
+++ VGG_CAM/utils.py
@@ -9,6 +9,8 @@ from PIL import Image
 import rasterio
 import numpy as np
 from torchvision import models, transforms
+import matplotlib.pyplot as plt
+from torch import Tensor
 
 
 class ImageDataset(Dataset):
@@ -35,3 +37,28 @@ class ImageDataset(Dataset):
 
         return image_tensor, label
 
+#https://www.fast.ai/
+#fastai code snippet
+class SaveFeatures():
+    features=None
+    def __init__(self,m): self.hook = m.register_forward_hook(self.hook_fn)
+    def hook_fn(self, module, input, output): self.features = output
+    def remove(self): self.hook.remove()
+
+# https://github.com/tony-mtz/CAM/blob/master/network/utils.py
+def accuracy(input:Tensor, targs:Tensor):
+    n = targs.shape[0]
+    input = input.argmax(dim=-1).view(n,-1)
+    targs = targs.view(n,-1)
+    return (input==targs).float().mean().cpu().detach().numpy()
+
+#https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+def imshow_transform(image_in, title=None):
+    """Imshow for Tensor."""
+    img = np.rollaxis(image_in.squeeze().cpu().detach().numpy(),0,3)
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    img = std * img + mean
+    img = np.clip(img, 0, 1)
+    plt.imshow(img)
+    return img
