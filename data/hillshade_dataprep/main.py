9ced03b1a0174afde419df900a0c042b81c3e75a
nflechner <60731786+nflechner@users.noreply.github.com>
2024-05-13 17:43:35 +0100

    new function which finds the RGB image in which hillshade image is

--- data/hillshade_dataprep/main.py
+++ data/hillshade_dataprep/main.py
@@ -15,7 +15,7 @@ import json
 import logging
 
 # functions 
-from functions import filter_imgs, Crop_tif_varsize
+from functions import get_RGB_match, Crop_tif_varsize
 
 ##################
 ## setup logger ##
@@ -46,6 +46,7 @@ config_paths = configs.get('paths', {})
 palsa_shapefile_path = config_paths.get('palsa_shapefile_path') # load shapefile path
 save_crops_dir = config_paths.get('save_crops_dir') # load directory with all tifs
 original_tif_dir = config_paths.get('original_tif_dir') # load directory with all tifs
+hillshade_tif_dir = config_paths.get('hillshade_tif_dir') # load directory with all tifs
 
 config_img = configs.get('image_info', {}) 
 dims = int(config_img.get('meters_per_axis')) 
@@ -58,23 +59,29 @@ logger.info('Configurations were loaded')
 
 logger.info('Starting to sample relevant TIF paths...')
 
-# extract tif file names which contain palsa
-palsa_tifs = os.listdir(original_tif_dir)
+hillshade_filenames = os.listdir(hillshade_tif_dir)
+"""NEED TO MAKE SURE THIS ONLY CONTAINS PALSA HILLSHADE FILES"""
 
-logger.info(f'{len(palsa_tifs)} TIF paths have been loaded!')
+logger.info(f'{len(hillshade_filenames)} TIF paths have been loaded!')
 logger.info('Starting to generate training samples from TIFs..')
 
 labels = {}
 
 # load palsa shape path
-for idx, img_name in enumerate(palsa_tifs):
-    img_name_code = img_name.split('.')[0]
-    img_path = os.path.join(original_tif_dir, img_name)
+for idx, img_name in enumerate(hillshade_filenames):
+    if str(img_name)[-4:] == ".tif":
+        RGB_tif = get_RGB_match(img_name, original_tif_dir)
 
-    cropping = Crop_tif_varsize(img_name_code, img_path, palsa_shapefile_path, save_crops_dir, dims, logger)
-    new_labels = cropping.forward()
-    labels = labels | new_labels
-    logger.info(f'Generated training samples from image {idx+1}/{len(palsa_tifs)}')
+# ___________________DONE TIL HERE_____________________________________
+
+
+        img_name_code = img_name.split('.')[0]
+        img_path = os.path.join(original_tif_dir, img_name)
+
+        cropping = Crop_tif_varsize(img_name_code, img_path, palsa_shapefile_path, save_crops_dir, dims, logger)
+        new_labels = cropping.forward()
+        labels = labels | new_labels
+        logger.info(f'Generated training samples from image {idx+1}/{len(palsa_tifs)}')
 
 label_df = pd.DataFrame.from_dict(labels, orient='index', columns = ['palsa_percentage'])
 label_df.to_csv(os.path.join(save_crops_dir, "palsa_labels.csv"))
