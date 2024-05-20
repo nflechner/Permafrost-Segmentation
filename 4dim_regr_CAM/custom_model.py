10f8197c12ac6081957f271bf07cdebd0a09be99
Nadja_Ubuntu_Circ <nadja.flechner@swissonline.ch>
2024-05-20 09:24:59 +0200

    rename and reorganize

---
+++ 4dim_regr_CAM/custom_model.py
@@ -0,0 +1,62 @@
+
+import torch.nn as nn
+import torch
+from torchvision import models
+
+class model_4D(nn.Module):
+
+    def __init__(self, init_weights=False): # INITWEIGHTS SHOULD BE TRUE 
+        super(model_4D, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(4, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 1, kernel_size=3, padding=1),
+            nn.BatchNorm2d(1),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2)
+                                )
+        self.classifier = nn.Sequential(
+                                nn.AdaptiveAvgPool2d(1),
+                                nn.Flatten(),
+                                nn.Sigmoid()
+                                )
+        if init_weights:
+            self._initialize_weights()
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
---
+++ 4dims_CNN/custom_model.py
@@ -0,0 +1,63 @@
+
+import torch.nn as nn
+import torch
+from torchvision import models
+
+class model_4D(nn.Module):
+
+    def __init__(self, init_weights=False): # INITWEIGHTS SHOULD BE TRUE 
+        super(model_4D, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(4, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 2, kernel_size=3, padding=1),
+            nn.BatchNorm2d(2),
+            nn.ReLU(inplace=True),            
+            nn.MaxPool2d(kernel_size=2, stride=2)
+                                )
+        self.classifier = nn.Sequential(
+                                nn.AdaptiveAvgPool2d(1),
+                                nn.Flatten()
+                                )
+        if init_weights:
+            self._initialize_weights()
+
+
+    def forward(self, x):
+        x = self.features(x)
+        #don't flatten 
+        #x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
