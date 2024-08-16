d3e9b0597ca7566a7be4eb63a962eb615ed50a70
nflechner <60731786+nflechner@users.noreply.github.com>
2024-08-16 08:43:13 +0200

    configure long gridsearch

--- Current_Version/pseudomasks/SWEEP_pseudomask-params.py
+++ Current_Version/pseudomasks/SWEEP_pseudomask-params.py
@@ -15,12 +15,12 @@ from model.pseudomask import Pseudomasks
 from model.train import ClassifierTrainLoop
 from utils.data_modules import ImageDataset, TestSet, filter_dataset
 
-##############################
-# hardcoded constant configs # 
-##############################
+############
+# SWEEP 1 # 
+############
 
 # model to be tested
-model = 'model:v31'
+model = 'classification_model:v88'
 
 artifact_path = f'nadjaflechner/VGG_CAMs/{model}'
 testset_dir = '/Users/nadja/Documents/UU/Thesis/Data/Verified_GTs'
@@ -29,14 +29,10 @@ normalize = True
 finetune = False
 std_from_mean = 0
 
-########################
-# define sweep configs #
-########################
-
 sweep_configuration = {
-    "method": "bayes",
+    "method": "random",
     "name": f"pseudomasks_{model}",
-    "metric": {"goal": "maximize", "name": "test_mean_jaccard"},
+    "metric": {"goal": "maximize", "name": "test_jaccard_palsa"},
     "parameters": {
         "cam_threshold_factor": {"max": 2.2, "min": 0.3},
         "overlap_threshold": {"max": 0.7, "min": 0.01},
@@ -46,10 +42,6 @@ sweep_configuration = {
 }
 
 sweep_id = wandb.sweep(sweep=sweep_configuration, project="VGG_CAMs")
-
-##############################
-# define train/test function #
-##############################
 
 test_set = TestSet(depth_layer, testset_dir, normalize)
 test_loader = DataLoader(test_set, batch_size=1, shuffle=True, num_workers=1)
@@ -68,7 +60,8 @@ def train_test_model():
     run = wandb.init(
         # Track hyperparameters and run metadata
         config={
-            "model": model
+            "model": model,
+            "normalize": normalize
             },
             tags=['OnlyPseudomaskParams']
     )
@@ -81,4 +74,250 @@ def train_test_model():
     pseudomask_generator.test_loop(test_loader)
 
 # Start sweep
-wandb.agent(sweep_id, function = train_test_model, count = 50)
+wandb.agent(sweep_id, function = train_test_model, count = 35)
+
+
+############
+# SWEEP 2 # 
+############
+
+# model to be tested
+model = 'classification_model:v61'
+
+artifact_path = f'nadjaflechner/VGG_CAMs/{model}'
+testset_dir = '/Users/nadja/Documents/UU/Thesis/Data/Verified_GTs'
+depth_layer = 'hs'
+normalize = False
+finetune = False
+std_from_mean = 0
+
+sweep_configuration = {
+    "method": "random",
+    "name": f"pseudomasks_{model}",
+    "metric": {"goal": "maximize", "name": "test_jaccard_palsa"},
+    "parameters": {
+        "cam_threshold_factor": {"max": 2.2, "min": 0.3},
+        "overlap_threshold": {"max": 0.7, "min": 0.01},
+        "snic_seeds": {"values": [20,40,60,80,100]},
+        "snic_compactness": {"max": 25, "min": 1}
+    },
+}
+
+sweep_id = wandb.sweep(sweep=sweep_configuration, project="VGG_CAMs")
+
+test_set = TestSet(depth_layer, testset_dir, normalize)
+test_loader = DataLoader(test_set, batch_size=1, shuffle=True, num_workers=1)
+
+pseudomask_generator = Pseudomasks(test_loader, None, None,
+                                    None, None, finetune, std_from_mean)
+
+api = wandb.Api()
+artifact = api.artifact(artifact_path, type='model')
+artifact_dir = artifact.download()
+state_dict = torch.load(f"{artifact_dir}/model.pth", map_location=torch.device('cpu'))
+pseudomask_generator.model_from_dict(state_dict)
+
+def train_test_model():
+
+    run = wandb.init(
+        # Track hyperparameters and run metadata
+        config={
+            "model": model,
+            "normalize": normalize
+            },
+            tags=['OnlyPseudomaskParams']
+    )
+
+    pseudomask_generator.cam_threshold = wandb.config.cam_threshold_factor
+    pseudomask_generator.overlap_threshold = wandb.config.overlap_threshold
+    pseudomask_generator.snic_seeds = wandb.config.snic_seeds
+    pseudomask_generator.snic_compactness = wandb.config.snic_compactness
+
+    pseudomask_generator.test_loop(test_loader)
+
+# Start sweep
+wandb.agent(sweep_id, function = train_test_model, count = 35)
+
+############
+# SWEEP 1 # 
+############
+
+# model to be tested
+model = 'classification_model:v61'
+
+artifact_path = f'nadjaflechner/VGG_CAMs/{model}'
+testset_dir = '/Users/nadja/Documents/UU/Thesis/Data/Verified_GTs'
+depth_layer = 'hs'
+normalize = True
+finetune = False
+std_from_mean = 0
+
+sweep_configuration = {
+    "method": "random",
+    "name": f"pseudomasks_{model}",
+    "metric": {"goal": "maximize", "name": "test_jaccard_palsa"},
+    "parameters": {
+        "cam_threshold_factor": {"max": 2.2, "min": 0.3},
+        "overlap_threshold": {"max": 0.7, "min": 0.01},
+        "snic_seeds": {"values": [20,40,60,80,100]},
+        "snic_compactness": {"max": 25, "min": 1}
+    },
+}
+
+sweep_id = wandb.sweep(sweep=sweep_configuration, project="VGG_CAMs")
+
+test_set = TestSet(depth_layer, testset_dir, normalize)
+test_loader = DataLoader(test_set, batch_size=1, shuffle=True, num_workers=1)
+
+pseudomask_generator = Pseudomasks(test_loader, None, None,
+                                    None, None, finetune, std_from_mean)
+
+api = wandb.Api()
+artifact = api.artifact(artifact_path, type='model')
+artifact_dir = artifact.download()
+state_dict = torch.load(f"{artifact_dir}/model.pth", map_location=torch.device('cpu'))
+pseudomask_generator.model_from_dict(state_dict)
+
+def train_test_model():
+
+    run = wandb.init(
+        # Track hyperparameters and run metadata
+        config={
+            "model": model,
+            "normalize": normalize
+            },
+            tags=['OnlyPseudomaskParams']
+    )
+
+    pseudomask_generator.cam_threshold = wandb.config.cam_threshold_factor
+    pseudomask_generator.overlap_threshold = wandb.config.overlap_threshold
+    pseudomask_generator.snic_seeds = wandb.config.snic_seeds
+    pseudomask_generator.snic_compactness = wandb.config.snic_compactness
+
+    pseudomask_generator.test_loop(test_loader)
+
+# Start sweep
+wandb.agent(sweep_id, function = train_test_model, count = 35)
+
+############
+# SWEEP 1 # 
+############
+
+# model to be tested
+model = 'classification_model:v45'
+
+artifact_path = f'nadjaflechner/VGG_CAMs/{model}'
+testset_dir = '/Users/nadja/Documents/UU/Thesis/Data/Verified_GTs'
+depth_layer = 'hs'
+normalize = True
+finetune = False
+std_from_mean = 0
+
+sweep_configuration = {
+    "method": "random",
+    "name": f"pseudomasks_{model}",
+    "metric": {"goal": "maximize", "name": "test_jaccard_palsa"},
+    "parameters": {
+        "cam_threshold_factor": {"max": 2.2, "min": 0.3},
+        "overlap_threshold": {"max": 0.7, "min": 0.01},
+        "snic_seeds": {"values": [20,40,60,80,100]},
+        "snic_compactness": {"max": 25, "min": 1}
+    },
+}
+
+sweep_id = wandb.sweep(sweep=sweep_configuration, project="VGG_CAMs")
+
+test_set = TestSet(depth_layer, testset_dir, normalize)
+test_loader = DataLoader(test_set, batch_size=1, shuffle=True, num_workers=1)
+
+pseudomask_generator = Pseudomasks(test_loader, None, None,
+                                    None, None, finetune, std_from_mean)
+
+api = wandb.Api()
+artifact = api.artifact(artifact_path, type='model')
+artifact_dir = artifact.download()
+state_dict = torch.load(f"{artifact_dir}/model.pth", map_location=torch.device('cpu'))
+pseudomask_generator.model_from_dict(state_dict)
+
+def train_test_model():
+
+    run = wandb.init(
+        # Track hyperparameters and run metadata
+        config={
+            "model": model,
+            "normalize": normalize
+            },
+            tags=['OnlyPseudomaskParams']
+    )
+
+    pseudomask_generator.cam_threshold = wandb.config.cam_threshold_factor
+    pseudomask_generator.overlap_threshold = wandb.config.overlap_threshold
+    pseudomask_generator.snic_seeds = wandb.config.snic_seeds
+    pseudomask_generator.snic_compactness = wandb.config.snic_compactness
+
+    pseudomask_generator.test_loop(test_loader)
+
+# Start sweep
+wandb.agent(sweep_id, function = train_test_model, count = 35)
+
+
+############
+# SWEEP 1 # 
+############
+
+# model to be tested
+model = 'classification_model:v67'
+
+artifact_path = f'nadjaflechner/VGG_CAMs/{model}'
+testset_dir = '/Users/nadja/Documents/UU/Thesis/Data/Verified_GTs'
+depth_layer = 'hs'
+normalize = True
+finetune = False
+std_from_mean = 0
+
+sweep_configuration = {
+    "method": "random",
+    "name": f"pseudomasks_{model}",
+    "metric": {"goal": "maximize", "name": "test_jaccard_palsa"},
+    "parameters": {
+        "cam_threshold_factor": {"max": 2.2, "min": 0.3},
+        "overlap_threshold": {"max": 0.7, "min": 0.01},
+        "snic_seeds": {"values": [20,40,60,80,100]},
+        "snic_compactness": {"max": 25, "min": 1}
+    },
+}
+
+sweep_id = wandb.sweep(sweep=sweep_configuration, project="VGG_CAMs")
+
+test_set = TestSet(depth_layer, testset_dir, normalize)
+test_loader = DataLoader(test_set, batch_size=1, shuffle=True, num_workers=1)
+
+pseudomask_generator = Pseudomasks(test_loader, None, None,
+                                    None, None, finetune, std_from_mean)
+
+api = wandb.Api()
+artifact = api.artifact(artifact_path, type='model')
+artifact_dir = artifact.download()
+state_dict = torch.load(f"{artifact_dir}/model.pth", map_location=torch.device('cpu'))
+pseudomask_generator.model_from_dict(state_dict)
+
+def train_test_model():
+
+    run = wandb.init(
+        # Track hyperparameters and run metadata
+        config={
+            "model": model,
+            "normalize": normalize
+            },
+            tags=['OnlyPseudomaskParams']
+    )
+
+    pseudomask_generator.cam_threshold = wandb.config.cam_threshold_factor
+    pseudomask_generator.overlap_threshold = wandb.config.overlap_threshold
+    pseudomask_generator.snic_seeds = wandb.config.snic_seeds
+    pseudomask_generator.snic_compactness = wandb.config.snic_compactness
+
+    pseudomask_generator.test_loop(test_loader)
+
+# Start sweep
+wandb.agent(sweep_id, function = train_test_model, count = 35)
